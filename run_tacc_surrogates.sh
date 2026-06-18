21f627415da7bfb8cb0c92d1fe529a3930e1b9ef
Luke Smith <smithl@tacc.utexas.edu>
2026-06-18 12:52:30 -0500

    minor tweak to multinode slurm script

--- run_tacc_surrogates.sh
+++ run_tacc_surrogates.sh
@@ -3,8 +3,8 @@
 #SBATCH -J run-epidemic-training               # Job name
 #SBATCH -o log.%j                         	 # Name of stdout output file (%j expands to jobId)
 #SBATCH -p gh-dev                       # Queue name
-#SBATCH -N 1                                 # Total number of nodes requested (56 cores/node)
-#SBATCH -n 1                                 # Total number of mpi tasks requested
+#SBATCH -N 2                                 # Total number of nodes requested (56 cores/node)
+#SBATCH -n 2                                 # Total number of mpi tasks requested
 #SBATCH -t 2:00:00                          # Run time (hh:mm:ss)
 #SBATCH -A XXXXXXXX							 # Project charge code
 
@@ -20,4 +20,4 @@ export PYTHONPATH=$SCRATCH/scripts/tacc-surrogates
 
 # Run training/evaluation (multinode)
 MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
-mpirun -np 2 --map-by ppr:1:node run_tacc_surrogates_idev.sh $MASTER_ADDR $SLURM_JOB_NUM_NODES
+mpirun -np $SLURM_NTASKS --map-by ppr:1:node run_tacc_surrogates_idev.sh $MASTER_ADDR $SLURM_JOB_NUM_NODES
